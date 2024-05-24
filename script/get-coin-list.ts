333e326fddfb79f61e8b399f2528a14b2b19baf3
crackcodesprogramloads <mikepadial@gmail.com>
2024-05-24 12:49:48 +1200

    Fix prisma schema and get-coin-list script

--- script/get-coin-list.ts
+++ script/get-coin-list.ts
@@ -7,19 +7,24 @@ const options = {
   headers: { accept: "application/json", "x-cg-demo-api-key": COINGECKO_API_KEY },
 };
 
-let page = 2;
 const max_page = 16;
 
-new Array(16).fill(0).forEach(async () => {
+new Array(max_page).fill(0).forEach(async (_, index) => {
   console.log("starting next loop");
+
+  const page = index + 1;
+
   fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=${page}`, options)
     .then((response) => response.json())
     .then(async (response) => {
       const data = response.map((coin: { name: string; symbol: string }) => {
         // insert row into model coin
+        const inWatchlist = false;
+
         return {
           name: coin.name,
           ticker: coin.symbol,
+          inWatchlist,
         };
       });
 
@@ -28,8 +33,7 @@ new Array(16).fill(0).forEach(async () => {
       });
 
       console.log("saved page: ", page);
-
-      page++;
+      console.log("data: ", JSON.stringify(data));
     })
     .catch((err) => {
       console.error(err.message);
