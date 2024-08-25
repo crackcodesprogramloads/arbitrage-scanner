60acee391262df7107bb8cb4046c0dc128e8e817
crackcodesprogramloads <mikepadial@gmail.com>
2024-08-25 10:36:04 -0400

    Change project name

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className="w-full h-16 flex flex-row items-center justify-around lg:justify-center text-gray-300 border-b border-gray-700 shadow-lg shadow-gray-800">
       <Link href="/">
-        <header className="pl-8 sm:pl-0 text-lg sm:text-3xl md:text-4xl">COIN NEWS AGGREGATOR</header>
+        <header className="pl-8 sm:pl-0 text-lg sm:text-3xl md:text-4xl">ARBITRAGE SCANNER</header>
       </Link>
       <AuthButton />
     </div>
