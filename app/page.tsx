eddf71638b2b7d708971be902bb776e08311acc3
crackcodesprogramloads <mikepadial@gmail.com>
2024-08-25 13:14:32 -0400

    Change home page text

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <main className="w-screen h-screen pr-10 pb-10 pl-10 flex flex-col items-center justify-center gap-10">
       <div className="flex flex-col items-center justify-center gap-2">
         <h1 className="py-1 text-[40px] sm:text-[45px] md:text-[52px] lg:text-[72px] text-transparent bg-clip-text bg-gradient-to-b from-stone-100 via-stone-100 to-stone-900">
-          Crypto news in one place
+          Find arbitrage opportunities
         </h1>
-        <h1 className="py-1 text-[40px] sm:text-[35px] md:text-[42px] lg:text-[52px] text-transparent bg-clip-text bg-gradient-to-b from-stone-100 via-stone-100 to-stone-900">
-          Add coins to your watchlist
-        </h1>
+        <h2 className="py-1 text-[40px] sm:text-[35px] md:text-[42px] lg:text-[52px] text-transparent bg-clip-text bg-gradient-to-b from-stone-100 via-stone-100 to-stone-900">
+          Between multiple crypto exchanges
+        </h2>
       </div>
       <SignUpButton />
     </main>
